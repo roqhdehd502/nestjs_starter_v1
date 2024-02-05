a5222b9c36d1ae8d5249658a1264ab088d64e80c
MinwooNa <natorque5552@gmail.com>
2024-02-05 15:11:35 +0900

    dev: 스웨거 문서 셋업

--- src/main.ts
+++ src/main.ts
@@ -1,11 +1,18 @@
 import { NestFactory } from '@nestjs/core';
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 import { AppModule } from './modules/app.module';
 
 async function bootstrap() {
-  // const app = await NestFactory.create(AppModule);
-  // await app.listen(4000);
   const app = await NestFactory.create(AppModule, { cors: true });
+  const config = new DocumentBuilder()
+    .setTitle('NestJS Starter V1')
+    .setDescription('NestJS V1 템플릿 입니다.')
+    .setVersion('1.0.0')
+    .build();
   app.setGlobalPrefix('v1');
+
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('v1/docs', app, document);
   await app.listen(4000);
 }
 bootstrap();
