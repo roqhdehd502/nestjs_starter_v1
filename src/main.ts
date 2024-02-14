ff3026503af536d653e61cc60a13370af69f9cb4
MinwooNa <natorque5552@gmail.com>
2024-02-14 14:09:09 +0900

    dev: 스웨거 아이콘 2차 설정

--- src/main.ts
+++ src/main.ts
@@ -29,7 +29,6 @@ async function bootstrap() {
   const document = SwaggerModule.createDocument(app, options);
   SwaggerModule.setup('v1/docs', app, document, {
     customSiteTitle: name,
-    customfavIcon: 'https://avatars.githubusercontent.com/u/6936373?s=200&v=4',
     customJs: [
       'https://cdnjs.cloudflare.com/ajax/libs/swagger-ui/4.15.5/swagger-ui-bundle.min.js',
       'https://cdnjs.cloudflare.com/ajax/libs/swagger-ui/4.15.5/swagger-ui-standalone-preset.min.js',
