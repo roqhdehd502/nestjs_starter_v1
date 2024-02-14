dbd9ef8596b1cf94bb1ce0386c8507a4788b629b
MinwooNa <natorque5552@gmail.com>
2024-02-14 13:00:08 +0900

    fix: 임시 기능 처리

--- src/controllers/app.controller.ts
+++ src/controllers/app.controller.ts
@@ -1,7 +1,22 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Get } from '@nestjs/common';
+import { ApiOperation, ApiResponse, ApiTags } from '@nestjs/swagger';
 import { AppService } from '../services/app.service';
 
+@ApiTags('test')
 @Controller()
 export class AppController {
   constructor(private readonly appService: AppService) {}
+
+  @Get()
+  @ApiOperation({ summary: '게시글 목록', description: '게시글 목록 불러오기' })
+  @ApiResponse({ status: 200, description: '성공' })
+  @ApiResponse({ status: 500, description: '내부 에러' })
+  async getPostList() {
+    return [
+      {
+        id: 12,
+        title: 'title',
+      },
+    ];
+  }
 }
