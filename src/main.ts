dcecfcbd6cf5f0044c20a8259c819e251a43a1de
MinwooNa <natorque5552@gmail.com>
2024-02-14 14:06:13 +0900

    fix: 스웨거 커스텀 css 적용

--- src/main.ts
+++ src/main.ts
@@ -27,7 +27,19 @@ async function bootstrap() {
     .build();
   app.setGlobalPrefix('v1');
   const document = SwaggerModule.createDocument(app, options);
-  SwaggerModule.setup('v1/docs', app, document);
+  SwaggerModule.setup('v1/docs', app, document, {
+    customSiteTitle: name,
+    customfavIcon: 'https://avatars.githubusercontent.com/u/6936373?s=200&v=4',
+    customJs: [
+      'https://cdnjs.cloudflare.com/ajax/libs/swagger-ui/4.15.5/swagger-ui-bundle.min.js',
+      'https://cdnjs.cloudflare.com/ajax/libs/swagger-ui/4.15.5/swagger-ui-standalone-preset.min.js',
+    ],
+    customCssUrl: [
+      'https://cdnjs.cloudflare.com/ajax/libs/swagger-ui/4.15.5/swagger-ui.min.css',
+      'https://cdnjs.cloudflare.com/ajax/libs/swagger-ui/4.15.5/swagger-ui-standalone-preset.min.css',
+      'https://cdnjs.cloudflare.com/ajax/libs/swagger-ui/4.15.5/swagger-ui.css',
+    ],
+  });
 
   // 앱 포트 적용
   await app.listen(process.env.PORT || 4000);
